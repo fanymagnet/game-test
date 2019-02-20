43c152a6bdbf561476534dd777da702e8472760e
Sergey Ivanov <fanymagnet@yandex.com>
2019-02-20 10:10:07 +0300

    Добавление тестового пользователя

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -19,11 +19,21 @@ class m130524_201442_init extends Migration
             'password_hash' => $this->string()->notNull(),
             'password_reset_token' => $this->string()->unique(),
             'email' => $this->string()->notNull()->unique(),
-
+            'bonus' => $this->float()->defaultValue(0)->notNull()->comment('Баллы пользователя'),
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
         ], $tableOptions);
+
+        $this->insert('user', [
+            'username' => 'admin',
+            'auth_key' => 'auth_key',
+            'password_hash' => '$2y$13$kiCUcY8Mz/tYmftpuy8pn.DB4ZwKzx7XGGoKdowUspbYQxRydScSi', // 127000
+            'password_reset_token' => 'password_reset_token',
+            'email' => 'test@test.ru',
+            'created_at' => 1000,
+            'updated_at' => 1000
+        ]);
     }
 
     public function down()
